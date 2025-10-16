aeecd1999ae6a62484f12f578b2460a969fe090f
Luca Battesini <lucabattesini1@gmail.com>
2025-10-16 16:10:15 -0300

    Creating menu

---
+++ Models/User.cs
@@ -0,0 +1,8 @@
+namespace Model
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+    }
+}
---
+++ Utils/Menu.cs
@@ -0,0 +1,17 @@
+namespace Utils
+{
+    public class Menu
+    {
+        public void Run()
+        {
+            Console.WriteLine("WELCOME TO THE LIBRARY");
+            Console.WriteLine("======================");
+
+            Console.WriteLine("1- Book's list");
+            Console.WriteLine("2- Lend book");
+            Console.WriteLine("3- Return book");
+            Console.WriteLine("4- Manage users");
+            Console.WriteLine("7- Exit");
+        }
+    }
+}
