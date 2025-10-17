89a42de62c0a8ae0338f7429815d2034de8eeba6
Luca Battesini <lucabattesini1@gmail.com>
2025-10-17 16:55:06 -0300

    Creating book menu

--- Utils/Menu.cs
+++ Utils/Menu.cs
@@ -2,8 +2,13 @@ namespace Utils
 {
     public class Menu
     {
-        public void Run()
+        public string BookName { get; private set; }
+        public string BookDescription { get; private set; }
+        public Dictionary<string, string> Book = new Dictionary<string, string>();
+
+        public void MainMenu()
         {
+            Console.Clear();
             Console.WriteLine("WELCOME TO THE LIBRARY");
             Console.WriteLine("======================");
 
@@ -12,6 +17,37 @@ namespace Utils
             Console.WriteLine("3- Return book");
             Console.WriteLine("4- Manage users");
             Console.WriteLine("7- Exit");
+
+            int Answer = Console.ReadLine();
+            return Answer;
+        }
+
+        public void ManageBook(string Action)
+        {
+            Console.Clear();
+            Console.WriteLine("Write down the book's name");
+            string BookName = Console.ReadLine();
+
+            Console.Clear();
+            Console.WriteLine("Write down the book's name");
+            string BookDescription = Console.ReadLine();
+
+            Console.Clear();
+            Console.WriteLine($"Name: {BookName}");
+            Console.WriteLine($"Description: {BookDescription}");
+            Console.WriteLine("Please, write Y if this is information is correct");
+            int Answer = Console.ReadLine();
+
+            if (Answer == y || Answer == Y)
+            {
+                Book["name"] = BookName;
+                Book["description"] = BookName;
+                return Book; Action;
+            }
+            else
+            {
+                ManageBook("lend");
+            }
         }
     }
 }
