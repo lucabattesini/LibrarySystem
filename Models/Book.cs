526f9d93c690dc02132d94789d29cd39bf6d9ee5
Luca Battesini <lucabattesini1@gmail.com>
2025-10-17 16:57:22 -0300

    Adding fields to book

--- Models/Book.cs
+++ Models/Book.cs
@@ -2,6 +2,13 @@ namespace Model
 {
     public class Book
     {
-        
+        public string BookName { get; protected set; }
+        public string BookDescription { get; protected set; }
+
+        public Book(string name, string description)
+        {
+            BookName = name;
+            BookDescription = description;
+        }
     }
 }
